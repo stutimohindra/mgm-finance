d5c663c2621d94dac05e27c2412f2e2a2f62ba9e
stutimohindra <stuti.mohindra@omio.com>
2020-05-02 16:12:18 +0200

    header

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './CustomButton.css';
 
 const CustomButton = (props) => {
-  const calculateEMI = () => {
-    console.log("sjhdjkshjkh")
-  }
   return (
     <button className={`${props.buttonName}`} onClick={props.calculateEMI}>
       <div className={`${props.name}`}>{props.text}</div>
--- src/components/HeaderMenuItems/HeaderMenuItems.js
+++ src/components/HeaderMenuItems/HeaderMenuItems.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './HeaderMenuItems.css';
 import mainLogo from '../../images/mainLogo.jpg';
-import hamburger from '../../images/hamburger.svg';
 
 const HeaderMenuItems = () => {
   return (
@@ -11,24 +10,15 @@ const HeaderMenuItems = () => {
           src={mainLogo}
           alt="1"
         />
-        <div className="DropDownMenu">
-          <a href="#menu">
-            <img
-              src={hamburger}
-              alt="1"
-            />
-          </a>
-        </div>
       </div>
       <div className="HeaderMenuItems">
         <ul className="Menuitems">
           <li className="Items">Explore Loans</li>
-          <li className="Items">Properties</li>
           <li className="Items">Services</li>
           <li className="Items">About Us</li>
         </ul>
       </div>
-    </div>
+    </div >
   )
 }
 export default HeaderMenuItems;
