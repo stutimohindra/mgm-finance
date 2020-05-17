f742a90cd80fd47f66d9fe10f13e88f7d5f10e72
stutimohindra <stuti.mohindra@omio.com>
2020-05-17 18:09:34 +0200

    adding address

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -6,11 +6,15 @@ const ContactUs = () => {
   return (
     <div id="Contact-us-rectangle">
       <Banner text="CONTACT US" name="Contact-us"></Banner>
-      <Banner text="186, Powai Plaza A Premises,
-      Hiranandani Gardens, Powai, Mumbai - 400076" name="Address"></Banner>
-      <Banner text="Phone: 022 6090 9996" name="Phone">
+      <Banner text="
+      Branch 1 : Lakhni Galaxy Chs Ltd, A303,
+      Plot No 83, Sector 15 CBD Belapur, Navi Mumbai - 400614
+      Branch 2 : 
+      Hno 2566A, Mukt Ashram Street,
+      Jagat Nagar, Basti Jodelwal, Ludhiana - 141007" name="Address"></Banner>
+      <Banner text="Phone: +91 97804 00243" name="Phone">
       </Banner>
-      <Banner text="Email: contact@loanstreet.in" name="Email">
+      <Banner text="Email: sales.mgmfinance@gmail.com" name="Email">
       </Banner>
       <div className="Line"></div>
       <Banner text="Copyright 2016 . All Rights Reserved. Terms & Conditions" name="Copyright"></Banner>
